aaba999b8317637d7cb7ac98d3300366c87865e3
EliaMagris <elia.magris@libero.it>
2023-02-02 15:05:57 +0100

    esercizio completato, rotte vue create e collegate

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,9 +26,16 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-import App from "./views/App";
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import router from './routes.js'
+
+import App from './views/App';
 
 const app = new Vue({
     el: '#root',
-    render: (h) => h(App),
+    router,
+    render: h => h(App),
 });
